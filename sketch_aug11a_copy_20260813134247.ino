7240b54f72f8d7ad53825c039bdeb701fc6ca512
CoolGuy123 <jtohman175@gmail.com>
2026-08-13 15:12:15 -0400

    Add files via upload
    added comments

--- sketch_aug11a_copy_20260813134247.ino
+++ sketch_aug11a_copy_20260813134247.ino
@@ -5,10 +5,11 @@ const char* password = "YOUR_WIFI_PASSWORD";
 const char* ssid = "YOUR_WIFI_NAME";
 
 Adafruit_NeoPixel strip(1, 8, NEO_GRB + NEO_KHZ800);
-
+// Create web server on HTTP port 80
 WiFiServer server(80);
 
 void setup() {
+  // Initialize the RGB LED
   strip.begin();
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -35,10 +36,12 @@ void loop() {
         while(client.available() == 0) {
           delay(500);
         }
+        // Read the HTTP request from the browser
         while (client.available()) {
           requestbyte = client.read();
           request +=  requestbyte;
         }
+        // Turn the LED ON or OFF with a RED color based on the requested URL
         if (request.startsWith("GET /led/on")) {
             strip.setPixelColor(0, strip.Color(255, 0, 0));
             strip.show();
@@ -47,7 +50,7 @@ void loop() {
             strip.setPixelColor(0, strip.Color(0, 0, 0));
             strip.show();
           }
-
+        // Gather information to display on the webpage
         String html;
         unsigned long uptime;
         uptime = millis() / 1000;
@@ -69,6 +72,8 @@ void loop() {
 
         int wifiStrength;
         wifiStrength = WiFi.RSSI();
+
+        // Build the HTML Webpage
 
         Serial.println(request);
         html = "<h1>ESP 32 WEB SERVER</h1> <p>Hello from my ESP32</p> <p>Uptime: ";
@@ -95,6 +100,7 @@ void loop() {
         html += "<form action=\"/led/on\" method = \"GET\">  <button type=\"submit\"> Turn on LED! </button> </form>";
         html += "<form action=\"/led/off\" method = \"GET\">  <button type=\"submit\"> Turn off LED! </button> </form>";
         
+        // Send the HTTP response and webpage to the browser
 
         client.print("HTTP/1.1 200 OK \r\n");
         client.print("Content-Type: text/html \r\n");
